f779bac3fe23adebeb4e445da595cc7ca06e0725
freekeycalendar <fkcdev11@gmail.com>
2024-09-23 23:04:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const RESPONSE =
 
 
 function getRows() {
-  // let url = 'https://api.sheety.co/1e98435928ea803a2e7aa06d00608900/fkcSocialCalendar/sheet1';
+  // let url = 'https://api.sheety.co/1e98435928ea803a2e7aa06d00608900/fkcSocialCalendar/events';
   // fetch(url)
   // .then((response) => response.json())
   // .then(json => {
@@ -29,7 +29,7 @@ function getRows() {
 
 
 function addRow() {
-  let url = 'https://api.sheety.co/1e98435928ea803a2e7aa06d00608900/fkcSocialCalendar/sheet1';
+  let url = 'https://api.sheety.co/1e98435928ea803a2e7aa06d00608900/fkcSocialCalendar/events';
   let body = {
     event: {
         "event": "amazing hangout",
