130c92740f20028405e7c644860e10ec6cf74e40
freekeycalendar <fkcdev11@gmail.com>
2024-09-23 17:29:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -83,3 +83,29 @@ function batchGetValues(spreadsheetId, _ranges, callback) {
   }
 }
 
+function buttonClick() {
+      let valueInputOption;
+      let values = ["i'm", "cool"]
+        let range = 
+      "C4:C5";
+    // Additional rows ..
+  const body = {
+    values: values,
+  };
+  try {
+    gapi.client.sheets.spreadsheets.values.update({
+      spreadsheetId: spreadsheetId,
+      range: range,
+      valueInputOption: valueInputOption,
+      resource: body,
+    }).then((response) => {
+      const result = response.result;
+      console.log(`${result.updatedCells} cells updated.`);
+      if (callback) callback(response);
+    });
+  } catch (err) {
+    document.getElementById('content').innerText = err.message;
+    return;
+  }
+}
+
