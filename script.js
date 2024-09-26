0608991a5fb7c7f448ac5150aa0f2299906692b7
freekeycalendar <fkcdev11@gmail.com>
2024-09-26 16:42:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,14 @@
-
-const RESPONSE = 
-[
-    {
-        "event": "Cool people concert",
-        "date": "9/28",
-        "time": "8pm",
-        "location": "The Greek",
-        "image link": "https://cdn.prod.website-files.com/654be393d1f855d893dfbd53/6664ae41dc4bf349f1438ee4_3c62edc0-3d9f-4fcb-bfc7-905a0275059d-p-1600.png",
-        "description": "This will be a really cool concert that happens at the greek. if you don't come you're a loser",
-        "id": 2
-    },
-    {
-        "event": "lame concert",
-        "date": "9/30",
-        "location": "my basement",
-        "image link": "https://cdn.prod.website-files.com/654be393d1f855d893dfbd53/6639af43e09b142b1467c81c_charlie_poster_out_of_time.png",
-        "description": "This concert is honestly gonna suck, idk why you would come",
-        "id": 3
-    },
-    {
-        "event": "suspicious concert",
-        "date": "11/1",
-        "location": "somewhere",
-        "id": 4
-    },
-]
-
-
 function getRows() {
   let url = 'https://api.sheety.co/1e98435928ea803a2e7aa06d00608900/fkcSocialCalendar/events';
   fetch(url)
   .then((response) => response.json())
   .then(json => {
     // Do something with the data
-    return json.events;
+    renderRows(json.events);
   });
-  // return RESPONSE;
+  // renderRows(RESPONSE);
 }
 
-
-function addRow() {
-  let url = 'https://api.sheety.co/1e98435928ea803a2e7aa06d00608900/fkcSocialCalendar/events';
-  let body = {
-    event: {
-        "event": "amazing hangout",
-        "date": "9/30",
-        "location": "the alley",
-    },
-  }
-    
-  fetch(url, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(body)
-  })
-  .then((response) => response.json())
-  .then(json => {
-    // Do something with object
-    renderRows(json.sheet1);
-  })
-  .catch((err) => {
-      console.log(err);
-  });
-}
 
 function renderRows(rows) {
     for (let i = 0; i < rows.length; i++) {
@@ -135,4 +79,68 @@ function maybeAddImage(node, imagelink) {
     }
 }
 
+// actually do the thing
+
 getRows();
+
+
+
+
+
+//dont need this
+
+
+function addRow() {
+  let url = 'https://api.sheety.co/1e98435928ea803a2e7aa06d00608900/fkcSocialCalendar/events';
+  let body = {
+    event: {
+        "event": "amazing hangout",
+        "date": "9/30",
+        "location": "the alley",
+    },
+  }
+    
+  fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+  .then((response) => response.json())
+  .then(json => {
+    // Do something with object
+    renderRows(json.sheet1);
+  })
+  .catch((err) => {
+      console.log(err);
+  });
+}
+
+
+const RESPONSE = 
+[
+    {
+        "event": "Cool people concert",
+        "date": "9/28",
+        "time": "8pm",
+        "location": "The Greek",
+        "image link": "https://cdn.prod.website-files.com/654be393d1f855d893dfbd53/6664ae41dc4bf349f1438ee4_3c62edc0-3d9f-4fcb-bfc7-905a0275059d-p-1600.png",
+        "description": "This will be a really cool concert that happens at the greek. if you don't come you're a loser",
+        "id": 2
+    },
+    {
+        "event": "lame concert",
+        "date": "9/30",
+        "location": "my basement",
+        "image link": "https://cdn.prod.website-files.com/654be393d1f855d893dfbd53/6639af43e09b142b1467c81c_charlie_poster_out_of_time.png",
+        "description": "This concert is honestly gonna suck, idk why you would come",
+        "id": 3
+    },
+    {
+        "event": "suspicious concert",
+        "date": "11/1",
+        "location": "somewhere",
+        "id": 4
+    },
+]
