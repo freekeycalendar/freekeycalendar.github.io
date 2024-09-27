94b711a64b2f2b44068d9f82b612f32fb7978fb6
freekeycalendar <fkcdev11@gmail.com>
2024-09-27 13:15:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function getRows() {
     renderRows(RESPONSE);
     //sheety
   } else {
-    let url = 'https://api.sheety.co/1e98435928ea803a2e7aa06d00608900/fkcSocialCalendar/events';
+    let url = 'https://api.sheety.co/1e98435928ea803a2e7aa06d00608900/fkcSocialCalendar/events/';
     fetch(url)
     .then((response) => response.json())
     .then(json => {
